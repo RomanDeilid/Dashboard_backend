a1e4e554cb2893c0946bf552feaef71163b6af44
Roman_Deilid <roma.deilid31@gmail.com>
2024-10-27 20:24:38 +0300

    edit endpoint sheets and addit status

--- src/sheets/sheet.repositories.ts
+++ src/sheets/sheet.repositories.ts
@@ -4,14 +4,21 @@ import { CreateSheetDto } from './dto/createSheetDto';
 import { UpdateSheetDto } from './dto/updateSheetDto';
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
 export class SheetRepository extends Repository<Sheet> {
   constructor(private dataSource: DataSource) {
     super(Sheet, dataSource.createEntityManager());
   }
 
-  public async findAll(): Promise<Sheet[]> {
-    return await this.find({});
+  public async findAll(companyId:number): Promise<Sheet[]> {
+    const  response= await  this.createQueryBuilder('Sheet')
+        .leftJoin('Sheet.company', 'company')
+        .where('company.id = :id', { id: companyId})
+        .getMany();
+
+    return response;
+
   }
 
   public async findById(sheetId: number): Promise<Sheet> {
@@ -19,6 +26,7 @@ export class SheetRepository extends Repository<Sheet> {
   }
 
   public async createItem(sheetDto: CreateSheetDto): Promise<Sheet> {
+
     const sheet = this.create(sheetDto);
 
     return this.save(sheet);
@@ -26,11 +34,25 @@ export class SheetRepository extends Repository<Sheet> {
 
   public async updateById(
     sheetId: number,
-    { name, description }: UpdateSheetDto
+    { name, description,status,company }: UpdateSheetDto
   ): Promise<number> {
     const update = await this.update(sheetId, {
       name: name,
+      status: status,
+      company:company,
       description: description,
+    });
+
+    return update.affected;
+  }
+
+
+  public async updateStatusById(
+      sheetId: number,
+      { name, description,status }: UpdateSheetDto
+  ): Promise<number> {
+    const update = await this.update(sheetId, {
+      status: status,
     });
 
     return update.affected;
